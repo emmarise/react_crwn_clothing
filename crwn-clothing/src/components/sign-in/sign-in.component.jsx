1d4f50df5140f7052eab548d9c98139d4f665486
Furong Tian <tian.fu@husky.neu.edu>
2020-07-29 10:16:29 -0400

    added redux, and implement userReducer and userActions to header and app component

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -55,7 +55,7 @@ class SignIn extends React.Component{
                     label='password' required />
                     <div className = 'buttons'>
                         <CustomButton type="submit">Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
                             {' '}
                             Sign in with Google{' '}
                         </CustomButton>
