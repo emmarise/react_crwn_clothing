8df914f41ffd0aba00f5426e2c0338d72752e21c
Furong Tian <tianfurong@gmail.com>
2020-08-05 17:32:05 -0400

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- crwn-clothing/src/redux/shop/shop.selectors.js
+++ crwn-clothing/src/redux/shop/shop.selectors.js
@@ -1,11 +1,15 @@
 import { createSelector } from 'reselect';
 
+
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
