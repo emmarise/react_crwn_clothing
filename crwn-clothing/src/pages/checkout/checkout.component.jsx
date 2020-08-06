ed68d94751b28d514eede4620568e75f64e9ec17
Furong Tian <tianfurong@gmail.com>
2020-08-06 11:55:19 -0400

    updating code with stripe integration

--- crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import {createStructuredSelector} from 'reselect'
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors'
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss'
 
@@ -31,8 +32,14 @@ const CheckoutPage = ({cartItems, total}) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
         <div className= 'total'>
-    <span>TOTAL: ${total}</span>
+            TOTAL: ${total}
         </div>
+        <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
